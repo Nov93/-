e0506b69ef1f6b0b2748eec8785db08abf89201d
Nov93 <dima.veronika93@gmail.com>
2025-01-12 15:52:16 +0300

    Create module_4_1.py

---
+++ module_4_1.py
@@ -0,0 +1,10 @@
+from fake_math import divide as fake
+result1=fake(8,4)
+print(result1)
+result2=fake(8,0)
+print(result2)
+from true_math import divide as true
+result3=true(10,0)
+print(result3)
+result4=true(10,2)
+print(result4)
